bdef08fdc7bb0d441e15fe3bb425e7fbe2b302d6
mcdavid95 <mcdavidemereuwa95@gmail.com>
2018-08-03 22:22:30 +0100

    ch(models): add schema for User collection
    - create User collection schema
    
    - start Product collection schema

---
+++ models/Product.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true ,
+    trim: true
+  },
+  quantity: { 
+    type: Number,
+    required: true
+  },
+  price_per_unit: {
+    type: Number,
+    required: true 
+  },
+  supplier: {
+    type: Schema.Types.ObjectId,
+    ref: 'Supplier'
+  },
+  description: Date,
+  passwordToken: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date }
+});
+
+const SALT_WORK_FACTOR = 10;
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
